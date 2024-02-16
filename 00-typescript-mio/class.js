a2e37e821c66d55fa8a84d76610abc33ba946e5c
Emilio Prieto Uclés <71763147+EmilioUA@users.noreply.github.com>
2024-02-16 10:23:32 +0100

    Develop branch for deploy

--- 00-typescript-mio/class.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-class Employee {
-}
-let john = new Employee();
-john.id = 1;
-john.name = "John";
-john.address = "Highway 71";
-console.log(john);
--- 01-typescript-intro/src/main.ts
+++
@@ -1,8 +0,0 @@
-import './style.css'
-import './topics/07-import-export'
-
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <h1> Hola mundo <h1>
-  </div>
-`
