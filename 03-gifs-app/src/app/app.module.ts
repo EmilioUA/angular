5e82940eb47d215a6a21866423b0eab0182f4248
Emilio Prieto Uclés <71763147+EmilioUA@users.noreply.github.com>
2024-02-21 20:58:16 +0100

    End of section 7

--- 03-gifs-app/src/app/app.module.ts
+++ 03-gifs-app/src/app/app.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http'
 
 import { AppRoutingModule } from './app-routing.module';
+import { GifsModule } from './gifs/gifs.module';
+import { SharedModule } from './shared/shared.module';
+
 import { AppComponent } from './app.component';
+
 
 @NgModule({
   declarations: [
@@ -10,7 +15,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    AppRoutingModule,
+    GifsModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
