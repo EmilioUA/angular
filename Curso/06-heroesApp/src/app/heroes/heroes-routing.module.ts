4892441d78eed3e9f02b9a7b8a1a5085b0ad56a3
EmilioUA <71763147+EmilioUA@users.noreply.github.com>
2024-08-27 00:34:36 +0200

    End of section 13
    Start heroes app

---
+++ Curso/06-heroesApp/src/app/heroes/heroes-routing.module.ts
@@ -0,0 +1,47 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
+import { ListPageComponent } from './pages/list-page/list-page.component';
+import { NewHeroPageComponent } from './pages/new-hero-page/new-hero-page.component';
+import { SearchPageComponent } from './pages/search-page/search-page.component';
+import { HeroPageComponent } from './pages/hero-page/hero-page.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: LayoutPageComponent,
+    children: [
+      {
+        path: 'list',
+        component: ListPageComponent,
+      },
+      {
+        path: 'new-hero',
+        component: NewHeroPageComponent,
+      },
+      {
+        path: 'search',
+        component: SearchPageComponent,
+      },
+      {
+        path: 'edit/:id',
+        component: NewHeroPageComponent,
+      },
+      {
+        path: ':id',
+        component: HeroPageComponent,
+      },
+      {
+        path: '**',
+        redirectTo: 'list',
+      },
+    ],
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class HeroesRoutingModule {}
